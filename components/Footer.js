44abb0ebe7df83d057cb4b13c63217695cad54eb
Sanna Mäkinen <sanna.makinen@gmail.com>
2021-03-10 15:55:03 +0200

    Let's hide the portfolio logo from screen readers, that has been previously missed

--- components/Footer.js
+++ components/Footer.js
@@ -93,7 +93,7 @@ const Footer = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <span className="portfolio-icon"></span>
+          <span className="portfolio-icon" aria-hidden="true"></span>
           My portfolio
         </a>
         <div className="links">
